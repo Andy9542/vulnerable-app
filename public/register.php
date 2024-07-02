5fc76378b53c33a4baacf0abf5c1e61d3aa86016
Andrey Iakovlev <andrew42ru@icloud.com>
2024-07-02 12:27:07 +0000

    Update register.php

--- public/register.php
+++ public/register.php
@@ -1,28 +1,52 @@
 <?php
-// /var/www/vulnerable-app/public/register.php
-include_once '../includes/auth.php';
+session_start();
+include_once '../config.php';
+
+// Если пользователь уже авторизован, перенаправляем его на главную страницу
+if (isset($_SESSION['user'])) {
+    header("Location: index.php");
+    exit();
+}
+
+$db_connection = pg_connect("host=".DB_HOST." dbname=".DB_NAME." user=".DB_USER." password=".DB_PASS);
+
+$message = '';
+
+function register($username, $password, $email) {
+    global $db_connection;
+    $hashed_password = md5($password); // Использование MD5 вместо более безопасных методов
+    $query = "INSERT INTO users (username, password, email) VALUES ('$username', '$hashed_password', '$email')";
+    return pg_query($db_connection, $query);
+}
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
     $email = $_POST['email'];
+    
     if (register($username, $password, $email)) {
-        header('Location: login.php');
-        exit;
+        $message = "Registration successful. You can now <a href='login.php'>login</a>.";
     } else {
-        $error = "Registration failed";
+        $message = "Registration failed. Please try again.";
     }
 }
 ?>
+
 <html>
+<head>
+    <title>Register</title>
+</head>
 <body>
     <h2>Register</h2>
-    <?php if (isset($error)) echo "<p>$error</p>"; ?>
+    <?php if ($message): ?>
+        <p><?php echo $message; ?></p>
+    <?php endif; ?>
     <form method="POST">
-        Username: <input type="text" name="username"><br>
-        Password: <input type="password" name="password"><br>
-        Email: <input type="email" name="email"><br>
+        Username: <input type="text" name="username" required><br>
+        Password: <input type="password" name="password" required><br>
+        Email: <input type="email" name="email" required><br>
         <input type="submit" value="Register">
     </form>
+    <p>Already have an account? <a href="login.php">Login here</a></p>
 </body>
 </html>
